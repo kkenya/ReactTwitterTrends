1a4ddd437b8cd260a1d8a85ae5b911bbc6089b41
kawatsu kenya <3982ne@gmail.com>
2018-05-25 23:38:09 +0900

    add tablelist

---
+++ app/javascript/bundles/TwitterTrends/components/ShowTrends.jsx
@@ -0,0 +1,193 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+const actionStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
+
+class TablePaginationActions extends Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
+
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      // Math.max => 第一引数と第二引数を評価し、大きい方を返す
+      // Math.ceil => 引数として与えた数以上の最小の引数を返す
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+const TablePaginationActionsWrapped = withStyles(actionStyles, { withTheme: true })(TablePaginationActions);
+
+const styles = theme => ({
+  root: {
+    width: '80%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 500,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+  tableCell: {
+    textAlign: 'right',
+  },
+});
+
+class CustomPaginationActionsTable extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = ({
+      data: this.props.data,
+      page: 0,
+      rowsPerPage: 10,
+    });
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = (event) => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { data, page, rowsPerPage } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+    return (
+      <Grid container justify="center" spacing={8}>
+      <Grid item xs={11}>
+        <Paper>
+          <div className={classes.tableWrapper}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Trend</TableCell>
+                  <TableCell className={classes.tableCell}>Tweet Volume</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(n => {
+                  return (
+                    <TableRow key={n.id}>
+                      <TableCell component="th" scope="row">
+                        {n.name}
+                      </TableCell>
+                      <TableCell numeric>{n.tweet_volume}</TableCell>
+                    </TableRow>
+                  );
+                })}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 48 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    colSpan={3}
+                    count={data.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationActionsWrapped}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </div>
+        </Paper>
+      </Grid>
+      </Grid>
+    );
+  }
+}
+
+CustomPaginationActionsTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CustomPaginationActionsTable);
+
--- app/javascript/bundles/TwitterTrends/components/TwitterTrends.jsx
+++ app/javascript/bundles/TwitterTrends/components/TwitterTrends.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
+import ShowTrends from './ShowTrends';
+import ShowHeader from './ShowHeader';
 
 const styles = theme => ({
   root: {
@@ -32,26 +33,6 @@ class TwitterTrends extends Component {
     };
     console.log(this.props.trends);
   }
-
-  renderHeader = ({ classes }) => {
-    const styles = {
-      root: {
-        flexGrow: 1,
-      },
-    };
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              Title
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  };
 
   renderTrends = (trends) => {
     const { classes } = this.props;
@@ -83,23 +64,23 @@ class TwitterTrends extends Component {
   };
 
   render() {
-    const { classes } = this.props;
-    const header = this.renderHeader(this.props);
-    const trendsList = this.renderTrends(this.state.trends);
+    const { trends, classes } = this.props;
+    const trendsList = this.renderTrends(trends);
 
     return (
       <React.Fragment>
         <CssBaseline />
         <Grid container className={classes.root} spacing={16}>
-          {header}
+          <ShowHeader />
           <Grid item xs={12}>
-            <Card className={classes.card}>
-              <CardContent>
-                <Typography variant="headline" component="h2">
-                  Twitter Trends
-                </Typography>
-              </CardContent>
-            </Card>
+            {/*<Card className={classes.card}>*/}
+              {/*<CardContent>*/}
+                {/*<Typography variant="headline" component="h2">*/}
+                  {/*Twitter Trends*/}
+                {/*</Typography>*/}
+              {/*</CardContent>*/}
+            {/*</Card>*/}
+            <ShowTrends data={trends}/>
           </Grid>
           {trendsList}
         </Grid>
